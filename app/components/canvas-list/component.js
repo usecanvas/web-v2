bfcf031ca7a375d5fffc1b25978431376278d42a
Max Schoening <max@max.wtf>
2016-09-15 13:11:07 -0700

    Add basic canvas-list

---
+++ app/components/canvas-list/component.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  localClass: ['canvas-list'],
+
+  canvases: [
+    {
+      title: 'Title',
+      summary: 'This is a cute summary of the canvas.'
+    },
+    {
+      title: 'Title',
+      summary: 'This is a cute summary of the canvas.'
+    },
+    {
+      title: 'Title',
+      summary: 'This is a cute summary of the canvas.'
+    },
+    {
+      title: 'Title',
+      summary: 'This is a cute summary of the canvas.'
+    },
+    {
+      title: 'Title',
+      summary: 'This is a cute summary of the canvas.'
+    }
+  ]
+});
