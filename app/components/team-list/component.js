cf1916474d70ebba299ca4066f13e6e526c0104e
Jonathan Clem <jotclem@gmail.com>
2016-09-15 17:43:31 -0700

    Display Slack team images

--- app/components/team-list/component.js
+++ app/components/team-list/component.js
@@ -1,37 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  localClassNames: ['team-list'],
-  teams: [
-    {
-      'id': 'T12345',
-      'name': 'My Team',
-      'domain': 'example',
-      'emailDomain': '',
-      'icon': {
-        'image34': '',
-        'image44': '',
-        'image68': '',
-        'image88': '',
-        'image102': '',
-        'image132': '',
-        'imageDefault': true
-      }
-    },
-    {
-      'id': 'T12345',
-      'name': 'My Team',
-      'domain': 'example',
-      'emailDomain': '',
-      'icon': {
-        'image34': '',
-        'image44': '',
-        'image68': '',
-        'image88': '',
-        'image102': '',
-        'image132': '',
-        'imageDefault': true
-      }
-    }
-  ]
+  localClassNames: ['team-list']
 });
--- app/models/team.js
+++ app/models/team.js
@@ -1,10 +1,16 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 const { attr } = DS;
+const { computed } = Ember;
 
 export default DS.Model.extend({
   name: attr(),
   imageUrl: attr(),
   insertedAt: attr(),
-  updatedAt: attr()
+  updatedAt: attr(),
+
+  image88: computed('imageUrl', function() {
+    return this.get('imageUrl').replace(/_original\.png$/, '_88.png');
+  })
 });
