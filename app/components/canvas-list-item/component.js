aff05e3c662019d1bcb1a20cc40268852eb4d9d9
Max Schoening <max@max.wtf>
2016-09-20 16:16:58 -0700

    Add custom icon and title badge for templates

--- app/components/canvas-list-item/component.js
+++ app/components/canvas-list-item/component.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   localClassNames: ['canvas-list-item'],
+  localClassNameBindings: ['canvas.isTemplate:is-template'],
 
   actions: {
     onClickDelete(evt) {
