0d4f4b0744ca4e921cdf44449c8790a7c0d413dc
Jonathan Clem <jonathan@jclem.net>
2016-09-27 15:18:48 -0400

    User current account for user in nav

--- app/components/team-nav/component.js
+++ app/components/team-nav/component.js
@@ -1,5 +1,9 @@
 import Ember from 'ember';
 
+const { computed } = Ember;
+
 export default Ember.Component.extend({
-  localClassNames: ['team-nav']
+  localClassNames: ['team-nav'],
+  currentAccount: Ember.inject.service(),
+  user: computed.readOnly('currentAccount.currentUser')
 });
