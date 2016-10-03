70d886adf30dbdeda0cf55ec6a80409f5ee772f3
Max Schoening <max@max.wtf>
2016-10-03 12:48:41 -0700

    Add filter to canvas lsit

---
+++ app/components/canvas-list-filter/component.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  localClassNames: ['canvas-list-filter'],
+
+  channels: [
+    'apple',
+    'bango',
+    'bingo',
+    'foobar',
+    'general',
+    'watercooler'
+  ]
+});
