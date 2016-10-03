6f2386ece092e8df21d883c13f7b30b3032cde60
Jonathan Clem <jonathan@jclem.net>
2016-10-03 15:51:04 -0400

    Add Add to Slack route

--- app/components/add-to-slack/component.js
+++ app/components/add-to-slack/component.js
@@ -5,6 +5,14 @@ import OAuth from 'canvas-web/mixins/oauth';
 export default Ember.Component.extend(OAuth, {
   clientID: ENV.slackClientID,
   endpoint: 'https://slack.com/oauth/authorize',
-  redirectURL: ENV.slackRedirectURI,
-  scope: 'incoming-webhook commands bot'.w()
+  redirectURL: ENV.slackAddRedirectURI,
+  scope: `
+    bot
+    channels:read
+    chat:write:bot
+    commands
+    team:read
+    users:read
+    `.w(),
+  state: 'add'
 });
