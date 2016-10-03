c4557607bf9a896b6a5cd5b16e2c17b3a2c2131e
Max Schoening <max@max.wtf>
2016-10-03 10:52:28 -0700

    Add add-to-slack button

--- app/components/add-to-slack/component.js
+++ app/components/add-to-slack/component.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
+import OAuth from 'canvas-web/mixins/oauth';
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(OAuth, {
+  clientID: ENV.slackClientID,
+  endpoint: 'https://slack.com/oauth/authorize',
+  redirectURL: ENV.slackRedirectURI,
+  scope: 'incoming-webhook commands bot'.w()
 });
