48c457b8196f6c2a46d2428d97496397632cffe1
Max Schoening <max@max.wtf>
2016-10-07 16:13:42 -0700

    Focus filter bar when it shows up

--- app/components/canvas-block-filter/component.js
+++ app/components/canvas-block-filter/component.js
@@ -1,8 +1,15 @@
 import Ember from 'ember';
 
+const { $, on } = Ember;
+
 export default Ember.Component.extend({
   filterTerm: '',
+  inputClass: 'js-input',
   localClassNames: ['canvas-block-filter'],
+
+  focusInput: on('didInsertElement', function() {
+    $(`.${this.get('inputClass')}`).focus();
+  }),
 
   actions: {
     close() {
