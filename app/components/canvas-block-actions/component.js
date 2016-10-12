dcfaccf9cdcd8e19dc259d7a0bc052958beda854
Max Schoening <max@max.wtf>
2016-10-11 18:19:40 -0700

    Add canvas-block-actions with clipboard code

---
+++ app/components/canvas-block-actions/component.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+const { computed } = Ember;
+
+export default Ember.Component.extend({
+  clipboardText: computed('canvas.id', 'block.id', function() {
+    const { protocol, hostname } = window.location;
+    const port = window.location.port ?  `:${window.location.port}` : '';
+
+    const canvasId = this.get('canvas.id');
+    const blockId = this.get('block.id');
+
+    return `${protocol}//${hostname}${port}/${canvasId}#${blockId}`;
+  })
+});
