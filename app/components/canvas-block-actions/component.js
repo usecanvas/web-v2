291b3f554f66140a89e7d16e3e2604da8f1aa2ff
Max Schoening <max@max.wtf>
2016-10-18 12:12:27 -0400

    Fix block URL for link copying

--- app/components/canvas-block-actions/component.js
+++ app/components/canvas-block-actions/component.js
@@ -5,13 +5,14 @@ const { computed } = Ember;
 export default Ember.Component.extend({
   localClassNames: ['canvas-block-actions'],
 
-  clipboardText: computed('canvas.id', 'block.id', function() {
+  clipboardText: computed('canvas.id', 'canvas.team.domain', 'block.id', function() {
     const { protocol, hostname } = window.location;
     const port = window.location.port ?  `:${window.location.port}` : '';
 
+    const team = this.get('canvas.team.domain');
     const canvasId = this.get('canvas.id');
     const blockId = this.get('block.id');
 
-    return `${protocol}//${hostname}${port}/${canvasId}#${blockId}`;
+    return `${protocol}//${hostname}${port}/${team}/${canvasId}#${blockId}`;
   })
 });
