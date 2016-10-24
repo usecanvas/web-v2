edd3df2e8cbce3f59eaabb83c42e2bba3f24bc9d
Olivia Briggs <ofbriggs@gmail.com>
2016-10-24 13:14:14 -0400

    Refactor controller code into route component and fix issue where creating a new canvas adds it to the canvas list before transitioning

--- app/controllers/team/index.js
+++ app/controllers/team/index.js
@@ -4,24 +4,5 @@ const { computed } = Ember;
 
 export default Ember.Controller.extend({
   queryParams: ['channel'],
-  channel: null,
-
-  filteredCanvases: computed(
-    'channel',
-    'model.channels.[]',
-    'model.canvases.@each.slackChannelIds', function() {
-      const canvases = this.get('model.canvases');
-      const channel = this.get('model.channels')
-                          .findBy('name', this.get('channel'));
-
-      if (!channel) return canvases;
-
-      return canvases.filter(canvas => {
-        return canvas.get('slackChannelIds').includes(channel.get('id'));
-      });
-    }),
-
-  channelModel: computed('channel', 'model.channels.[]', function() {
-    return this.get('model.channels').findBy('name', this.get('channel'));
-  })
+  channel: null
 });
