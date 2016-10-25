7554cb6d07a0b374eb9dee74e9829165b312887f
Max Schoening <max@max.wtf>
2016-10-25 11:55:46 -0700

    Add dummy public access toggle

--- app/components/canvas-settings-route/component.js
+++ app/components/canvas-settings-route/component.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
+const { w } = Ember.String;
+
 export default Ember.Component.extend({
+  allowPublicAccess: false,
+  permissions: w('view edit'),
+
+  actions: {
+    togglePublicAccess() {
+      console.log('Toggling public access...');
+      this.toggleProperty('allowPublicAccess');
+    }
+  }
 });
