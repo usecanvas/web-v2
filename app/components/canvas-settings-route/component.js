aeeec077dcf737a65ecb6740b779b4e9deb1d7ff
Olivia Briggs <ofbriggs@gmail.com>
2016-10-27 15:43:46 -0400

    Persist and get permission state from server

--- app/components/canvas-settings-route/component.js
+++ app/components/canvas-settings-route/component.js
@@ -1,15 +1,50 @@
 import Ember from 'ember';
+import { task } from 'ember-concurrency';
 
+const { computed, on } = Ember;
 const { w } = Ember.String;
 
 export default Ember.Component.extend({
   allowPublicAccess: false,
   permissions: w('view edit'),
+  permission: 'view',
+  setInitialPermissionState: on('init', function() {
+    const state = this.get('canvas.linkAccess');
+    if (state === 'none') return;
+    this.set('allowPublicAccess', true);
+    this.set('permission',
+      state === 'edit' ? 'edit' : 'view');
+  }),
+  currentPermissionState: computed('allowPublicAccess',
+                                   'permission', function() {
+    const { allowPublicAccess, permission } =
+      this.getProperties('allowPublicAccess', 'permission');
+    if (!allowPublicAccess) {
+      return 'none';
+    } else if (permission === 'view') {
+      return 'read';
+    }
+    return 'edit';
+  }),
+
+  persistPermissionState: task(function *() {
+    const newState = this.get('currentPermissionState');
+    const canvas = this.get('canvas');
+    if (canvas.get('linkAccess') !== newState) {
+      canvas.set('linkAccess', newState);
+      yield canvas.save();
+    }
+  }).keepLatest(),
 
   actions: {
     togglePublicAccess() {
-      console.log('Toggling public access...');
       this.toggleProperty('allowPublicAccess');
+      this.get('persistPermissionState').perform();
+    },
+
+    updatePermission({ target: { value } }) {
+      this.set('permission', value);
+      this.get('persistPermissionState').perform();
     }
   }
 });
--- app/routes/team/canvas/settings.js
+++ app/routes/team/canvas/settings.js
@@ -1,3 +1,7 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({});
+export default Ember.Route.extend({
+  model() {
+    return this.modelFor('team.canvas');
+  }
+});
