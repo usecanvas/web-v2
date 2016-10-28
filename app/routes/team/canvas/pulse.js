e2c09090ec0b13ca6bf9fe55b831f0cfb064f3b1
Olivia Briggs <ofbriggs@gmail.com>
2016-10-28 15:26:37 -0400

    Fix broken model destructuring in pulse route

--- app/routes/team/canvas/pulse.js
+++ app/routes/team/canvas/pulse.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    const { canvas } = this.modelFor('team.canvas');
+    const canvas = this.modelFor('team.canvas');
 
     return canvas.get('pulseEvents').then(pulseEvents => {
       return [this.get('store').createRecord('pulseEvent', {
