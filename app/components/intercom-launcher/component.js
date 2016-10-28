758375b2d4f1321d4dcb259a36277b14d1ce2276
Max Schoening <max@max.wtf>
2016-10-28 10:34:22 -0700

    Use elementId to set Intercom ID

--- app/components/intercom-launcher/component.js
+++ app/components/intercom-launcher/component.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 const INTERCOM_APP_ID = 'htxi7bqv';
 
 export default Ember.Component.extend({
-  attributeBindings: ['href', 'id'],
+  attributeBindings: ['href'],
+  elementId: 'custom-intercom-link',
   href: `mailto:${INTERCOM_APP_ID}@incoming.intercom.io`,
-  id: 'custom-intercom-link',
   tagName: 'a'
 });
