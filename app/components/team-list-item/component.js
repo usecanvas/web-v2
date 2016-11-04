eb77361562b755a0523f0c0579f775cc009eaeb9
Max Schoening <max@max.wtf>
2016-11-04 12:37:43 -0700

    Set fallback avatar for teams that don't provide an avatar URL

--- app/components/team-list-item/component.js
+++ app/components/team-list-item/component.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
+const { computed } = Ember;
+
 export default Ember.Component.extend({
-  localClassNames: ['team-list-item']
+  localClassNames: ['team-list-item'],
+
+  imageURL: computed('team.image88', function() {
+    return this.getWithDefault('team.image88',
+      '/images/personal-notes-avatar.png');
+  })
 });
