9410606f4ca23e4de793ae7f4c443722b8898583
Max Schoening <max@max.wtf>
2016-11-04 23:26:17 -0700

    Move canvasDownloadURL to model

--- app/components/canvas-title-actions/component.js
+++ app/components/canvas-title-actions/component.js
@@ -1,22 +1,9 @@
-import CanvasAdapter from 'canvas-web/adapters/canvas';
 import Ember from 'ember';
-import ENV from 'canvas-web/config/environment';
-
-const { computed } = Ember;
 
 export default Ember.Component.extend({
   localClassNames: ['canvas-title-actions'],
   menuIsShowing: false,
   router: Ember.inject.service(),
-
-  downloadURL: computed('canvas.team', 'canvas.id', function() {
-    const path = CanvasAdapter.create().urlForFindRecord(
-      this.get('canvas.id'),
-      this.get('canvas.constructor.modelName'),
-      { adapterOptions: { team: this.get('canvas.team') } });
-
-    return `${ENV.apiURL.replace(/\/v1\/$/, '')}${path}.canvas`;
-  }),
 
   actions: {
     deleteCanvas() {
