25c8b999be21358d2cdb0d7f145def93743483ac
Jonathan Clem <jonathan@jclem.net>
2016-11-09 10:43:22 -0500

    Add context menu, open dev tools in development

--- electron.js
+++ electron.js
@@ -1,4 +1,6 @@
-/* jshint node: true */
+/* eslint-env node */
+
+// eslint-disable-next-line strict
 'use strict';
 
 const electron             = require('electron');
@@ -9,17 +11,9 @@ const BrowserWindow        = electron.BrowserWindow;
 const dirname              = __dirname || path.resolve(path.dirname());
 const emberAppLocation     = `file://${dirname}/dist/index.html`;
 
+require('electron-context-menu')();
+
 let mainWindow = null;
-
-// Uncomment the lines below to enable Electron's crash reporter
-// For more information, see http://electron.atom.io/docs/api/crash-reporter/
-
-// electron.crashReporter.start({
-//     productName: 'YourName',
-//     companyName: 'YourCompany',
-//     submitURL: 'https://your-domain.com/url-to-submit',
-//     autoSubmit: true
-// });
 
 app.on('window-all-closed', function onWindowAllClosed() {
   if (process.platform !== 'darwin') {
@@ -37,10 +31,12 @@ app.on('ready', function onReady() {
    }
   });
 
-  delete mainWindow.module;
+  Reflect.deleteProperty(mainWindow, 'module');
 
   // If you want to open up dev tools programmatically, call
-  // mainWindow.openDevTools();
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.openDevTools();
+  }
 
   // By default, we'll open the Ember App by directly going to the
   // file system.
@@ -52,8 +48,14 @@ app.on('ready', function onReady() {
 
   // If a loading operation goes wrong, we'll send Electron back to
   // Ember App entry point
-  mainWindow.webContents.on('did-fail-load', (evt) => {
+  mainWindow.webContents.on('did-fail-load', _evt => {
     if (mainWindow.webContents.getURL() === 'http://localhost:4200/') {
+      mainWindow.loadURL(emberAppLocation);
+    }
+  });
+
+  mainWindow.webContents.on('will-navigate', (evt, url) => {
+    if (['https://pro.usecanvas.com/', 'http://localhost:4200'].includes(url)) {
       mainWindow.loadURL(emberAppLocation);
     }
   });
@@ -65,16 +67,19 @@ app.on('ready', function onReady() {
   });
 
   mainWindow.webContents.on('crashed', () => {
-    console.log('Your Ember app (or other code) in the main window has crashed.');
-    console.log('This is a serious issue that needs to be handled and/or debugged.');
+    console.warn(
+      'Your Ember app (or other code) in the main window has crashed.');
+    console.warn(
+      'This is a serious issue that needs to be handled and/or debugged.');
   });
 
   mainWindow.on('unresponsive', () => {
-    console.log('Your Ember app (or other code) has made the window unresponsive.');
+    console.warn(
+      'Your Ember app (or other code) has made the window unresponsive.');
   });
 
   mainWindow.on('responsive', () => {
-    console.log('The main window has become responsive again.');
+    console.warn('The main window has become responsive again.');
   });
 
   mainWindow.on('closed', () => {
@@ -95,22 +100,25 @@ app.on('ready', function onReady() {
 
   // Handle an unhandled error in the main thread
   //
-  // Note that 'uncaughtException' is a crude mechanism for exception handling intended to
-  // be used only as a last resort. The event should not be used as an equivalent to
-  // "On Error Resume Next". Unhandled exceptions inherently mean that an application is in
-  // an undefined state. Attempting to resume application code without properly recovering
-  // from the exception can cause additional unforeseen and unpredictable issues.
+  // Note that 'uncaughtException' is a crude mechanism for exception handling
+  // intended to be used only as a last resort. The event should not be used as
+  // an equivalent to "On Error Resume Next". Unhandled exceptions inherently
+  // mean that an application is in an undefined state. Attempting to resume
+  // application code without properly recovering from the exception can cause
+  // additional unforeseen and unpredictable issues.
   //
-  // Attempting to resume normally after an uncaught exception can be similar to pulling out
-  // of the power cord when upgrading a computer -- nine out of ten times nothing happens -
-  // but the 10th time, the system becomes corrupted.
+  // Attempting to resume normally after an uncaught exception can be similar to
+  // pulling out of the power cord when upgrading a computer -- nine out of ten
+  // times nothing happens - but the 10th time, the system becomes corrupted.
   //
-  // The correct use of 'uncaughtException' is to perform synchronous cleanup of allocated
-  // resources (e.g. file descriptors, handles, etc) before shutting down the process. It is
-  // not safe to resume normal operation after 'uncaughtException'.
-  process.on('uncaughtException', (err) => {
-    console.log('An exception in the main thread was not handled.');
-    console.log('This is a serious issue that needs to be handled and/or debugged.');
-    console.log(`Exception: ${err}`);
+  // The correct use of 'uncaughtException' is to perform synchronous cleanup of
+  // allocated resources (e.g. file descriptors, handles, etc) before shutting
+  // down the process. It is not safe to resume normal operation after
+  // 'uncaughtException'.
+  process.on('uncaughtException', err => {
+    console.warn('An exception in the main thread was not handled.');
+    console.warn(
+      'This is a serious issue that needs to be handled and/or debugged.');
+    console.warn(`Exception: ${err}`);
   });
 });
