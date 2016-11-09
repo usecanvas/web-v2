802ecbcdd4e70f87360b644360d8eafc25e9c999
Olivia Briggs <ofbriggs@gmail.com>
2016-11-09 12:57:03 -0500

    Fix image path for personal collection

--- app/components/team-list-item/component.js
+++ app/components/team-list-item/component.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import ENV from 'canvas-web/config/environment';
 
 const { computed } = Ember;
 
@@ -7,6 +8,6 @@ export default Ember.Component.extend({
 
   imageURL: computed('team.image88', function() {
     return this.getWithDefault('team.image88',
-      '/images/personal-notes-avatar.png');
+      `${ENV.rootURL}images/personal-notes-avatar.png`);
   })
 });
