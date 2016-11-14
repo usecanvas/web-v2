3e7e2bb0d4d0eff970f62db85040e672c84f271c
Max Schoening <max@max.wtf>
2016-11-14 14:24:03 -0800

    Replace ui-dropdown with ember-basic-dropdown

--- app/components/ui-dropdown/component.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-import UIDropdownMixin from 'canvas-web/mixins/ui-dropdown';
-
-export default Ember.Component.extend(UIDropdownMixin, {
-  localClassNames: ['ui-dropdown']
-});
