c74c97f52f6f1352eaeea1e2f84c7c5ce78d2597
Max Schoening <max@max.wtf>
2016-12-16 14:54:45 -0800

    Document ui-menu/item

--- app/components/ui-menu/item/component.js
+++ app/components/ui-menu/item/component.js
@@ -1,7 +1,41 @@
 import Ember from 'ember';
 
+/**
+ * Renders a menu item inside of a `ui-menu`.
+ *
+ * The item can be used to render:
+ *
+ * - A regular link tag (i.e. `<a href={{link}}></a>`)
+ * - A `link-to` Component
+ * - Or yield any component with a `click` action
+ *
+ * @class CanvasWeb.UIMenuItemComponent
+ * @extends Ember.Component
+ */
 export default Ember.Component.extend({
   localClassNames: ['ui-menu-item'],
+
+  /**
+   * @member {string} The download attribute for a `link`.
+   *
+   * <a href={{link}} download={{download}}>{{yield}}</a>
+   */
+  download: null,
+
+  /**
+   * @member {string} A URL that will render the `ui-menu` item as a link:
+   *
+   * <a href={{link}}>{{yield}}</a>
+   */
+  link: null,
+
+  /**
+   * @member {array} An array to be passted to a link-to component's `params`
+   *   property.
+   *
+   * {{link-to params=route}}{{yield}}{{/link-to}}
+   */
+  route: null,
 
   click() {
     const action = this.get('action');
