6918797abe71af8b7a26c4eef76ebd2e8be2294c
Max Schoening <max@max.wtf>
2016-12-15 23:30:09 -0800

    Add basic documentation for not-found controller

--- app/controllers/not-found.js
+++ app/controllers/not-found.js
@@ -2,7 +2,21 @@ import Ember from 'ember';
 
 const { computed, inject } = Ember;
 
+/**
+ * A controller for our 404 page to give us access to the current user session.
+ *
+ * @class CanvasWeb.NotFoundController
+ * @extends Ember.Controller
+ */
 export default Ember.Controller.extend({
+  /**
+   * @member {CanvasWeb.CurrentAccountService} A service exposing the current
+   *   user account
+   */
   currentAccount: inject.service(),
+
+  /**
+   * @member {boolean} Whether the current user is logged in
+   */
   loggedIn: computed.readOnly('currentAccount.loggedIn')
 });
