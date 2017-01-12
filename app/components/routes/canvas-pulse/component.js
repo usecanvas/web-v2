fd8b5986f24fc4601c6b67ed1f1b510648360d2d
Jonathan Clem <jonathan@jclem.net>
2017-01-12 17:36:05 -0500

    Make array more readable

--- app/components/routes/canvas-pulse/component.js
+++ app/components/routes/canvas-pulse/component.js
@@ -3,6 +3,12 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   localClassNames: ['route-canvas-pulse'],
   pulseEvents: Ember.computed(_ => []),
-  slackScopesForPulse: `bot channels:history channels:read chat:write:bot
-  commands team:read users:read`.w()
+  slackScopesForPulse: `
+    bot
+    channels:history
+    channels:read
+    chat:write:bot
+    commands
+    team:read
+    users:read`.w()
 });
