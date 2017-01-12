b0ce7e4f065d364bd3a12257314537d307648c7a
Jonathan Clem <jonathan@jclem.net>
2017-01-12 18:18:13 -0500

    Wip scope refactor

--- app/components/routes/canvas-pulse/component.js
+++ app/components/routes/canvas-pulse/component.js
@@ -3,12 +3,5 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   localClassNames: ['route-canvas-pulse'],
   pulseEvents: Ember.computed(_ => []),
-  slackScopesForPulse: `
-    bot
-    channels:history
-    channels:read
-    chat:write:bot
-    commands
-    team:read
-    users:read`.w()
+  slackScope: ['channels:history']
 });
--- app/components/slack-oauth/component.js
+++ app/components/slack-oauth/component.js
@@ -2,16 +2,58 @@ import ENV from 'canvas-web/config/environment';
 import Ember from 'ember';
 import OAuth from 'canvas-web/mixins/oauth';
 
+const { computed } = Ember;
+
+/**
+ * A component that can be used to initiate a Slack OAuth flow.
+ *
+ * @class CanvasWeb.SlackOAuthComponent
+ * @extends Ember.Component
+ */
 export default Ember.Component.extend(OAuth, {
-  clientID: ENV.slackClientID,
-  endpoint: 'https://slack.com/oauth/authorize',
-  redirectURL: ENV.slackAddRedirectURI,
-  scope: `bot
+  /**
+   * @member {Array<string>} A base set of non-invasive scopes that we request.
+   */
+  baseScope: `
+    bot
     channels:read
     chat:write:bot
     commands
     groups:read
     team:read
     users:read`.w(),
+
+  /**
+   * @member {?string} The Slack OAuth client ID
+   */
+  clientID: ENV.slackClientID,
+
+  /**
+   * @member {string} The Slack OAuth authorization endpoint URL
+   */
+  endpoint: 'https://slack.com/oauth/authorize',
+
+  /**
+   * @member {Array<string>} A set of extended invasive scopes to be added on
+   *   to the base OAuth scope set
+   */
+  extendedScope: [],
+
+  /**
+   * @member {?string} The redirect URI for the Slack OAuth client
+   */
+  redirectURL: ENV.slackAddRedirectURI,
+
+  /**
+   * @member {Array<string>} A list of scopes that will be requested.
+   */
+  scope: computed('baseScope.[]', 'extendedScope.[]', function() {
+    return this.get('baseScope').concat(this.get('extendedScope'));
+  }),
+
+  /**
+   * @member {string} A Slack OAuth state nonce—we are currently not making
+   *   good use of this.
+   */
   state: 'add'
 });
