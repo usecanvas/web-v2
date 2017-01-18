34cafd2fed5b8e3dc8b7024c1685ff11935d8ac8
Olivia Briggs <ofbriggs@gmail.com>
2017-01-18 13:27:45 -0500

    Prevent meta-a from bubbling up from comment-thread/new-item & add meta-return to submit

--- app/components/comment-thread/create-item/component.js
+++ app/components/comment-thread/create-item/component.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import Key from 'canvas-web/lib/key';
 import { task } from 'ember-concurrency';
 
 export default Ember.Component.extend({
@@ -7,5 +8,15 @@ export default Ember.Component.extend({
   submit: task(function *(content) {
     yield this.get('submitComment')(content);
     this.set('content', '');
-  }).drop()
+  }).drop(),
+
+  actions: {
+    commentKeyDown(_, evt) {
+      evt.stopPropagation();
+      const key = new Key(evt.originalEvent);
+      if (key.is('meta', 'return')) {
+        this.get('submit').perform(this.get('content'));
+      }
+    }
+  }
 });
