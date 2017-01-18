9c861f3a1fdc045664294176aa57133cc07584b3
Olivia Briggs <ofbriggs@gmail.com>
2017-01-18 12:21:19 -0500

    Commented findRecord to better explain need for override

--- app/adapters/block.js
+++ app/adapters/block.js
@@ -2,6 +2,10 @@ import ApplicationAdapter from './application';
 import Ember from 'ember';
 
 export default ApplicationAdapter.extend({
+  /**
+  * Return a payload client-side so block finds will not fail when
+  * the comment model requests the block through its relationship
+  */
   findRecord(_store, _type, id) {
     return Ember.RSVP.resolve({ data: { id, type: 'block' } });
   }
