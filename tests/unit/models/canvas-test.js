520a7ecf81d37df70aefe39efe2e9e1092f25a52
Olivia Briggs <ofbriggs@gmail.com>
2017-01-18 12:45:24 -0500

    Add missing model dependency for canvas model test

--- tests/unit/models/canvas-test.js
+++ tests/unit/models/canvas-test.js
@@ -2,7 +2,7 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('canvas', 'Unit | Model | canvas', {
   // Specify the other units that are required for this test.
-  needs: 'model:op model:pulseEvent model:team model:user'.w()
+  needs: 'model:comment model:op model:pulseEvent model:team model:user'.w()
 });
 
 test('it exists', function(assert) {
