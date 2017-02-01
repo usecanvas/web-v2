9e202ab0fd2220f93bba2b0dd35191d6cf60e129
Jonathan Clem <jonathan@jclem.net>
2017-02-01 16:56:42 -0500

    Make fuzzing clearer

--- app/initializers/sharedb-fuzz.js
+++ app/initializers/sharedb-fuzz.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import Base62UUID from 'canvas-editor/lib/base62-uuid';
 
 export default {
   name: 'sharedb-fuzz',
@@ -9,14 +10,24 @@ export default {
   }
 };
 
-function shareDBFuzz() {
-  const firstBlock = Ember.$('.canvas-block-paragraph-content').get(0);
+function shareDBFuzz(text) {
+  const firstBlock  = Ember.$('.canvas-block-paragraph-content').get(0);
   const $firstBlock = Ember.$(firstBlock);
-  $firstBlock.text(`${$firstBlock.text()}X `);
+
+  $firstBlock.text(`${$firstBlock.text()}${text} `);
   $firstBlock.trigger('input');
 }
 
 function shareDBFuzzBurst(interval = 10, duration = 5000) {
-  const intervalID = window.setInterval(shareDBFuzz, interval);
+  const id  = Base62UUID.generate();
+  let index = 0;
+
+  const intervalID = window.setInterval(_ => {
+    shareDBFuzz(`${id}.${index}`);
+    index += 1;
+  }, interval);
+
   setTimeout(_ => window.clearInterval(intervalID), duration);
+
+  console.info(`Fuzzing as: ${id}`); // eslint-disable-line no-console
 }
